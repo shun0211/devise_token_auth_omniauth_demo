51785aa7b80aa697f1db72d5907b832324ec6855
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2021-04-17 10:37:54 +0900

    デモ完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'User', at: 'auth',
+  controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
 end
